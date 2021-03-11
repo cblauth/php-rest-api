0089c9817ab1d85c2ecf2dcd269813c18a0fda42
Carlos <cmblauth@gmail.com>
2021-03-11 21:09:34 +0000

    validation

--- src/grade_process.php
+++ src/grade_process.php
@@ -7,31 +7,37 @@ require_once("student.php");
   
 class GradeProcess implements Api {
 
-    // using PHP 8 new contructor property
-    function __construct(public $counter = 0) {
+    function __construct() {
         $this->result = [];
         $this->request = "";
+        $this->counter = 0;
     }
      
     public function process(string $data) {
         
+        // only accepting POST requests
+        if ($_SERVER["REQUEST_METHOD"] != "POST") {
+            exit("[{\"error\" : \"invalid request method\"}]");
+        }  
+        
         $this->request = json_decode($data);       
         if ($this->request === null && json_last_error() !== JSON_ERROR_NONE) {
             exit("[{\"error\" : \"invalid json\"}]");
-        }             
+        }
+                     
         foreach($this->request as $elem) {
              
-            $counter ++;
+            $this->counter ++;
             
-            // validations to ensure the structure is valid                       
+            // validations to ensure the structure is valid                                             
             if ((!isset($elem->name)) OR (!isset($elem->grade))) {
-                exit("[{\"error\" : \"missing name or grade, element $counter\"}]");
+                exit("[{\"error\" : \"missing name or grade, element $this->counter\"}]");
             }             
             if(!(Validation::validateName($elem->name))) {
-                exit("[{\"error\": \"name invalid, element $counter\"}]");      
+                exit("[{\"error\": \"name invalid, element $this->counter\"}]");      
             }   
             if(!(Validation::validateGrade($elem->grade))) {
-                exit("[{\"error\" : \"grade invalid, element $counter\"}]");      
+                exit("[{\"error\" : \"grade invalid, element $this->counter\"}]");      
             }     
             
             // send grades to student class to be processed            
@@ -50,11 +56,7 @@ class GradeProcess implements Api {
 }  
   
 $data = file_get_contents("php://input");
-// only accepting POST requests
-if ($_SERVER["REQUEST_METHOD"] != "POST") {
-    exit("[{\"error\" : \"invalid request\"}]");
-}  
 $api = new GradeProcess();
 $api->process($data);
-  
+
 ?>
